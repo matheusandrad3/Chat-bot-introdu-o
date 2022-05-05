e8ae76459157edc825f0bf5f67f0d2765fcef0d5
matheusandrad3 <matheusandradedesouza@outlook.com>
2022-05-05 09:12:37 -0300

    05/05

---
+++ static/scripts/responses.js
@@ -0,0 +1,86 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "sim") {
+      return "que ótimo <br> Qual o seu nome?";
+    } else if (input == "sim e você?") {
+        return "estou bem, também! Obrigado por perguntar =) <br> Qual o seu nome?";
+    } else if (input == "sim e voce?") {
+        return "estou bem, também! Obrigado por perguntar <br> Qual o seu nome?";
+    }else if (input == "sim e voce") {
+        return "estou bem, também! Obrigado por perguntar <br> Qual o seu nome?";
+    }else if (input == "sim e vc") {
+        return "estou bem, também! Obrigado por perguntar <br> Qual o seu nome?";
+    }else if (input == "sim e vc?") {
+        return "estou bem, também! Obrigado por perguntar <br> Qual o seu nome?";
+    }
+
+
+    if (input == "Joao") {
+        return "The famous Jhons <br> Você gosta mais de... <br> Escalar ou jogar Poker?";
+    }else if (input == "joao") {
+        return "The famous Jhons <br> Você gosta mais de... <br> Escalar ou jogar Poker?";
+    }else if (input == "Joao Markus") {
+        return "The famous Jhons <br> Você gosta mais de... <br> Escalar ou jogar Poker?";
+    }else if (input == "Joao markus") {
+        return "The famous Jhons <br> Você gosta mais de... <br> Escalar ou jogar Poker?";
+    }else if (input == "joao markus") {
+        return "The famous Jhons <br> Você gosta mais de... <br> Escalar ou jogar Poker?";
+    }else if (input == "escalar") {
+        return "Nice, continue treinando <br> vai arrumar o que hoje?";
+    }else if (input == "poker") {
+        return "AHHHH... vocé é o the best player <br> vai arrumar o que hoje?";
+    }else if (input == "Poker") {
+        return "AHHHH... vocé é o the best player <br> vai arrumar o que hoje?";
+    }
+    else if (input == "Escalar") {
+        return "Nice, continue treinando <br> vai arrumar o que hoje";
+    }
+    else if (input == "jogar poker") {
+        return "AHHHH... vocé é o the best player <br> vai arrumar o que hoje?";
+    }
+    else if (input == "jogar") {
+        return "AAAAAU hoje tem FORRAAAA <br> GL MÁXIMA";
+    }else if (input == "trabalhar") {
+        return "AAAAAU hoje tem FORRAAAA <br> GL MÁXIMA";
+    }else if (input == "estudar") {
+        return "Pra cima deles Jotinha";
+    }else if (input == "nada") {
+        return "Se parar da dengue, pow!!! <br> Para de morcegar";
+    }
+
+    
+
+
+    if (input == "Brenda e o seu?") {
+        return "Hmmmm... a princesa do teteu <br> ele te ama <br> O que vc esta fazendo?";
+    } else if (input == "brenda e o seu?") {
+        return "Hmmmm... a princesa do teteu <br> ele te ama <br> O que vc esta fazendo?";
+    }else if (input == "brenda e o seu?") {
+        return "Hmmmm... a princesa do teteu <br> ele te ama <br> O que vc esta fazendo?";
+    }else if (input == "Brenda") {
+        return "Hmmmm... a princesa do teteu <br> ele te ama <br> O que vc esta fazendo?";
+    }else if (input == "brenda") {
+        return "Hmmmm... a princesa do teteu <br> ele te ama <br> O que vc esta fazendo?";
+    }
+    else if (input == "tita") {
+        return "Hmmmm... a princesa do teteu <br> ele te ama <br> O que vc esta fazendo?";
+    }else if (input == "bre") {
+        return "Hmmmm... a princesa do teteu <br> ele te ama <br> O que vc esta fazendo?";
+    }
+    else if (input == "trabalhando") {
+        return "Nice Work, gatita";
+    }
+    else if (input == "atoa") {
+        return "Se parar da dengue, pow!!! <br> Para de morcegar";
+    }
+    else if (input == "descansando") {
+        return "Se parar da dengue, pow!!! <br> Para de morcegar";
+    }
+    else {
+        return "tente de novo, por favor <br> ainda não estou 100% codificado";
+    }
+
+
+    
+}
+
